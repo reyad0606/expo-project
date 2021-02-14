86ca22d3cca3e5653678248ac86a8763e4cc4f07
RM <70502707+reyad0606@users.noreply.github.com>
2021-02-14 02:35:13 -0500

    Flatlist, Button, InputText, ID added

--- App.js
+++ App.js
@@ -1,24 +1,75 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+  FlatList,
+} from 'react-native';
 
 export default function App() {
-  const [output, setOutput] = useState('Hello Azra. Want some love ??');
+  // var uuid = require('react-native-uuid');
+
+  const [enteredGoal, setEnteredGoal] = useState('');
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText);
+  };
+
+  const addGoalHandler = () => {
+    // setCourseGoals([...courseGoals, enteredGoal]); // not 100% guranteed
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { id: Math.random().toString(), value: enteredGoal },
+    ]); //100% guranteed it will give the current state
+    // setEnteredGoal('');
+  };
   return (
-    <View style={styles.container}>
-      <Text>{output}</Text>
-      <Button
-        title='Change Text'
-        onPress={() => setOutput('Its changed. see !!')}
-      />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title='ADD' onPress={addGoalHandler} />
+      </View>
+      <View>
+        <FlatList
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals}
+          renderItem={(itemData) => (
+            <View style={styles.listItem}>
+              <Text>{itemData.item.value}</Text>
+            </View>
+          )}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
+  screen: { padding: 50 },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  textInput: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10,
+  },
+  listItem: {
+    padding: 10,
+    margin: 10,
+    backgroundColor: 'lightblue',
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
